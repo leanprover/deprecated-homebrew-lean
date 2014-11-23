1f1d1451ffc50de1e5ae8753baeee44e698cc3a1
Soonho Kong <soonhok@cs.cmu.edu>
2014-11-22 21:19:04 -0500

    Bottle: 0.2.0-git3523a70b4c8bd414991fdfcc5c763adb96ed045b

--- lean.rb
+++ lean.rb
@@ -5,11 +5,11 @@ class Lean < Formula
   url "https://github.com/leanprover/lean.git"
   version "0.2.0-git3523a70b4c8bd414991fdfcc5c763adb96ed045b"
 
-##BOTTLE_COMMENT##  bottle do
-##BOTTLE_COMMENT##    root_url 'https://leanprover.github.io/homebrew-lean'
-##BOTTLE_COMMENT##    sha1 '##BOTTLE_YOSEMITE_HASH##' => :yosemite
-##BOTTLE_COMMENT##    sha1 '##BOTTLE_MAVERICKS_HASH##' => :mavericks
-##BOTTLE_COMMENT##  end
+  bottle do
+    root_url 'https://leanprover.github.io/homebrew-lean'
+    sha1 '8c9f4af08a95d3d087f55679151605f727671984' => :yosemite
+    sha1 '##BOTTLE_MAVERICKS_HASH##' => :mavericks
+  end
 
   # Required
   depends_on 'gmp'
