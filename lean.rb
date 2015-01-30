ee5fe7ca779a4c05e24fbc1a44b9bdac585ec2bc
Soonho Kong <soonhok@cs.cmu.edu>
2015-01-30 13:15:12 -0500

    0.2.0-git1ee52a3ddc514b098ef6370ba3fd1da9c4f08bdd-10.10.2 [skip ci]

--- lean.rb
+++ lean.rb
@@ -3,7 +3,7 @@ require "formula"
 class Lean < Formula
   homepage "http://leanprover.github.io"
   url "https://github.com/leanprover/lean.git"
-  version "0.2.0-gitd98f04fd3aad3658c2d3649ffbf97c40732be992"
+  version "0.2.0-git1ee52a3ddc514b098ef6370ba3fd1da9c4f08bdd"
 
 ##BOTTLE_COMMENT##  bottle do
 ##BOTTLE_COMMENT##    root_url 'https://leanprover.github.io/homebrew-lean'
