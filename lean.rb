2fa74aa1e95e31cc9304dbaec768c1e2e08470b1
Soonho Kong <soonhok@cs.cmu.edu>
2015-02-10 21:59:25 -0500

    Update: 0.2.0.20150210213445.git9398b887cc7c7cc705f0e8552bd21b63817e9664

--- lean.rb
+++ lean.rb
@@ -3,12 +3,12 @@ require "formula"
 class Lean < Formula
   homepage "http://leanprover.github.io"
   url "https://github.com/leanprover/lean.git"
-  version "0.2.0.20150209212531.gitc3e7b1f8177a8fbe7a58be51b6050eac230b2c74"
+  version "0.2.0.20150210213445.git9398b887cc7c7cc705f0e8552bd21b63817e9664"
 
   bottle do
     root_url 'https://leanprover.github.io/homebrew-lean'
-    sha1 'b15e34ea22142860f0e54cc0548fffe967e2ee48' => :yosemite
-    sha1 '6cf9866d6ae0b5338dc342caf3c8b9f9a2b04ffc' => :mavericks
+    sha1 '04c5312e1c61cdb8bec38a58ac4ceeac67b99fdc' => :yosemite
+    sha1 '0bdb4ed1ffe8bb27c289369cb18bad84c45dc99c' => :mavericks
   end
 
   # Required
