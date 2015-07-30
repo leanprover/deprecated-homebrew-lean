26b6485a8b169ed86c2eb2ab6f4e23bd57611949
Soonho Kong <soonhok@cs.cmu.edu>
2015-07-30 08:41:23 -0700

    fix(check_bottle): fix sha1 check

--- check_bottle.sh
+++ check_bottle.sh
@@ -9,13 +9,13 @@ FORMULA_TEMPLATE=${FORMULA_NAME}.rb.template
 OSX_VERSION=`sw_vers -productVersion`
 #---------------------------------------------------
 
-VERSION_STRING=`brew info ${FORMULA_NAME} 2>&1 | head -n 1 | cut -d ' ' -f 3`
+VERSION_STRING=`brew info ${ORG_NAME}/${REPO_NAME}/${FORMULA_NAME} 2>&1 | head -n 1 | cut -d ' ' -f 3`
 echo ${VERSION_STRING}
 
 BOTTLE_FILE_YOSEMITE=${FORMULA_NAME}-${VERSION_STRING}.yosemite.bottle.tar.gz
-wget https://github.com/${ORG_NAME}/homebrew-${REPO_NAME}/blob/yosemite/${BOTTLE_FILE_YOSEMITE}?raw=true -O ${BOTTLE_FILE_YOSEMITE}
+wget https://github.com/${ORG_NAME}/homebrew-${REPO_NAME}/blob/gh-pages/${BOTTLE_FILE_YOSEMITE}?raw=true -O ${BOTTLE_FILE_YOSEMITE}
 YOSEMITE_FILE_HASH=`shasum ${BOTTLE_FILE_YOSEMITE} | cut -d ' ' -f 1`
-YOSEMITE_FORMULA_HASH=`grep "sha1" /usr/local/Library/Formula/${FORMULA_FILE} | grep "yosemite" | cut -d "'" -f 2`
+YOSEMITE_FORMULA_HASH=`grep "sha1" /usr/local/Library/Taps/${ORG_NAME}/homebrew-${REPO_NAME}/${FORMULA_FILE} | grep "yosemite" | cut -d "'" -f 2`
 
 echo "yosemite  hash : ${YOSEMITE_FILE_HASH} -- ${YOSEMITE_FORMULA_HASH}"
 
