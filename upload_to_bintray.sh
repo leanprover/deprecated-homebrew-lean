008073b838c14ab1bbca4d3895ed8cefaa3d7501
Soonho Kong <soonhok@cs.cmu.edu>
2015-12-01 14:11:30 -0500

    add upload_to_bintray.sh

---
+++ upload_to_bintray.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+ID=$1
+PASSWORD=$2
+VERSION=$3
+case `uname -r` in
+  14.*)
+    OSX_NAME="yosemite"
+    ;;
+  15.*)
+    OSX_NAME="el_capitan"
+    ;;
+  *)
+    OSX_NAME="unknown"
+    ;;
+esac
+
+BOTTLE_FILENAME=lean-${VERSION}.${OSX_NAME}.bottle.tar.gz
+BINTRAY_URL=https://api.bintray.com/content/lean/lean/lean
+
+
+if [ -e ${BOTTLE_FILENAME} ]
+then
+  curl -T ${BOTTLE_FILENAME} -u${ID}:${PASSWORD} ${BINTRAY_URL}/lean-${VERSION}/${BOTTLE_FILENAME}
+else 
+  echo "File not found: ${BOTTLE_FILENAME}"
+fi
