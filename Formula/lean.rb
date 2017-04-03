2c0e939fe208226b481058c3d3a788a4123f14b2
Josh Tilles <MerelyAPseudonym@gmail.com>
2017-04-03 08:37:16 -0400

    Appease RuboCop
    Addresses the following style violations:
    
    ```
    * `caveats method` (line 49) should be put before `test block` (line 42)
    * Formula should have a desc (Description).
    * `require "formula"` is now unnecessary
    * Use the `mv` Ruby method instead of `system "mv"`
    * Use the `rm` Ruby method instead of `system "rm"`
    * C: 9: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 10: col 12: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 11: col 12: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 13: col 1: Trailing whitespace detected
    * C: 15: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 16: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 17: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 18: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 19: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 19: col 21: Unnecessary spacing detected.
    * C: 21: col 14: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    * C: 21: col 21: Unnecessary spacing detected.
    * C: 30: col 11: Prefer double-quoted strings unless you need single quotes to avoid extra backslashes for escaping.
    ```

--- Formula/lean.rb
+++ Formula/lean.rb
@@ -1,24 +1,24 @@
-require "formula"
-
 class Lean < Formula
-  homepage "http://leanprover.github.io"
+  desc "Interactive, automatable, dependently-typed theorem prover"
+  homepage "https://leanprover.github.io"
   url "https://github.com/leanprover/lean.git"
   version "0.2.0.20160602210703.gitc73b2860d5211187e9aa1039d1a49dcabdca4292"
 
   bottle do
-    root_url 'https://dl.bintray.com/lean/lean'
-    sha256 '329689bfef46678547dca02ccc637ed7cd37f6831e0577aa75dc8af0c3231345' => :yosemite
-    sha256 'ac9fe7e4f2c1ea33dc6af5dada8c662728f350cad459c69cd833980a92d93fb1' => :el_capitan
+    root_url "https://dl.bintray.com/lean/lean"
+    sha256 "329689bfef46678547dca02ccc637ed7cd37f6831e0577aa75dc8af0c3231345" => :yosemite
+    sha256 "ac9fe7e4f2c1ea33dc6af5dada8c662728f350cad459c69cd833980a92d93fb1" => :el_capitan
   end
 
+  option "with-boost", "Compile using boost"
+
   # Required
-  depends_on 'gmp'
-  depends_on 'mpfr'
-  depends_on 'lua'
-  depends_on 'ninja'
-  depends_on 'cmake'            => :build
-  option     "with-boost", "Compile using boost"
-  depends_on 'boost'            => [:build, :optional]
+  depends_on "gmp"
+  depends_on "mpfr"
+  depends_on "lua"
+  depends_on "ninja"
+  depends_on "cmake" => :build
+  depends_on "boost" => [:build, :optional]
 
   def install
     args = ["-DCMAKE_INSTALL_PREFIX=#{prefix}",
@@ -27,23 +27,16 @@ class Lean < Formula
             "-GNinja",
             "-DLIBRARY_DIR=./"]
     args << "-DBOOST=ON" if build.with? "boost"
-    mkdir 'build' do
+    mkdir "build" do
       system "curl", "-O", "-L", "https://github.com/leanprover/emacs-dependencies/archive/master.zip"
       system "unzip", "master.zip"
-      system "mv", "emacs-dependencies-master", "../src/emacs/dependencies"
-      system "rm", "master.zip"
+      mv "emacs-dependencies-master", "../src/emacs/dependencies"
+      rm "master.zip"
       system "cmake", "../src", *args
       system "ninja", "clean"
       system "ninja"
       system "ninja", "install"
     end
-  end
-
-  test do
-    system "curl", "-O", "-L", "https://github.com/leanprover/lean/archive/master.zip"
-    system "unzip", "master.zip"
-    system "lean-master/tests/lean/test.sh", "#{bin}/lean"
-    system "lean-master/tests/lua/test.sh",  "#{bin}/lean"
   end
 
   def caveats; <<-EOS.undent
@@ -76,5 +69,12 @@ class Lean < Formula
       (add-to-list 'load-path (expand-file-name lean-emacs-path))
       (require 'lean-mode)
     EOS
+  end
+
+  test do
+    system "curl", "-O", "-L", "https://github.com/leanprover/lean/archive/master.zip"
+    system "unzip", "master.zip"
+    system "lean-master/tests/lean/test.sh", "#{bin}/lean"
+    system "lean-master/tests/lua/test.sh",  "#{bin}/lean"
   end
 end
